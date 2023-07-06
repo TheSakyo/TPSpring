fa266cbc04e07c65de362770681cbf880f98af06
TheSakyo <sacha.cardone@gmail.com>
2023-07-06 09:06:51 +0200

    Initial commit

--- src/main/java/fr/thesakyo/TPSpring/TpSpringApplication.java
+++ src/main/java/fr/thesakyo/TPSpring/TpSpringApplication.java
@@ -6,8 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TpSpringApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(TpSpringApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(TpSpringApplication.class, args); }
 }
