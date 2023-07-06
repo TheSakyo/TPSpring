c11728c056b55891941c2fc2fe07f52d09cd7fab
TheSakyo <sacha.cardone@gmail.com>
2023-07-06 16:01:31 +0200

    Initial commit

--- src/test/java/fr/thesakyo/TPSpring/controller/UserControllerTest.java
+++ src/test/java/fr/thesakyo/TPSpring/controller/UserControllerTest.java
@@ -1,7 +1,5 @@
 package fr.thesakyo.TPSpring.controller;
 
-import fr.thesakyo.TPSpring.model.User;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -13,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootTest
 @AutoConfigureMockMvc
